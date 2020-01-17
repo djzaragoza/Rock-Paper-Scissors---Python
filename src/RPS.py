c1d46e035105b1c06a5878f740eeff4de3c5004a
DJ Zaragoza <zaragoza0126@gmail.com>
2020-01-16 19:23:38 -0800

    finished game code

--- src/RPS.py
+++ src/RPS.py
@@ -24,3 +24,20 @@ outcomes = {
     }
 }
 
+while playing == True :
+    # players picking what they want 
+    player_choice = raw_input('Choose rock, paper, or scissor: ')
+    comp_choice = random.choice(choices)
+    
+    if outcomes[player_choice] [comp_choice] == 'win':
+        score += 1
+        print('You Win!', score )
+    elif outcomes[player_choice] [comp_choice] == 'tie':
+        print('You tied!', score )
+    elif outcomes[player_choice] [comp_choice] == 'lose':
+        print('You lose sucka!')
+        playing = False 
+        print('Your score: ', score )
+        
+        
+
