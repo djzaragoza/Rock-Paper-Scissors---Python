cc713b90b21e6fd2469b85df0c08ce06819c309c
DJ Zaragoza <zaragoza0126@gmail.com>
2020-01-16 19:14:22 -0800

    created RPS.py file

---
+++ src/RPS.py
@@ -0,0 +1,26 @@
+BuiltinImporter
+
+print('Welcome to Rock Paper Scissors!')
+
+choices = ['rock', 'paper', 'scissor']
+score = 0
+playing = True
+
+outcomes = {
+    'rock': {
+        'rock': 'tie',
+        'paper': 'lose',
+        'scissor': 'win'
+    },
+    'paper': {
+        'rock': 'win',
+        'paper': 'tie',
+        'scissor': lose
+    },
+    'scissor': {
+        'rock': 'lose',
+        'paper': 'win',
+        'scissor': 'tie'
+    }
+}
+
